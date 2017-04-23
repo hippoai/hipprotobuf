65b7923dc4a15447851fbdf129bf447020fc0669
selim <selim@wearkinetic.com>
2017-04-23 01:24:26 -0400

    make wrappers

--- empty.go
+++ empty.go
@@ -40,6 +40,24 @@ func GetFloat(in *google_protobuf.FloatValue) *float32 {
 	return &in.Value
 }
 
+// GetDouble returns nil or a pointer to the primitive value
+func GetDouble(in *google_protobuf.DoubleValue) *float64 {
+	if in == nil {
+		return nil
+	}
+
+	return &in.Value
+}
+
+// GetBytes returns nil or a pointer to the primitive value
+func GetBytes(in *google_protobuf.BytesValue) *[]byte {
+	if in == nil {
+		return nil
+	}
+
+	return &in.Value
+}
+
 // GetString returns nil or a pointer to the primitive value
 func GetString(in *google_protobuf.StringValue) *string {
 	if in == nil {
@@ -67,20 +85,101 @@ func GetUInt64(in *google_protobuf.UInt32Value) *uint32 {
 	return &in.Value
 }
 
-// GetDouble returns nil or a pointer to the primitive value
-func GetDouble(in *google_protobuf.DoubleValue) *float64 {
+// MakeBool makes a wrapper
+func MakeBool(in *bool) *google_protobuf.BoolValue {
 	if in == nil {
 		return nil
 	}
 
-	return &in.Value
+	return &google_protobuf.BoolValue{
+		Value: *in,
+	}
 }
 
-// GetBytes returns nil or a pointer to the primitive value
-func GetBytes(in *google_protobuf.BytesValue) *[]byte {
+// MakeInt32 makes a wrapper
+func MakeInt32(in *int32) *google_protobuf.Int32Value {
 	if in == nil {
 		return nil
 	}
 
-	return &in.Value
+	return &google_protobuf.Int32Value{
+		Value: *in,
+	}
+}
+
+// MakeInt64 makes a wrapper
+func MakeInt64(in *int64) *google_protobuf.Int64Value {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.Int64Value{
+		Value: *in,
+	}
+}
+
+// MakeFloat makes a wrapper
+func MakeFloat(in *float32) *google_protobuf.FloatValue {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.FloatValue{
+		Value: *in,
+	}
+}
+
+// MakeDouble makes a wrapper
+func MakeDouble(in *float64) *google_protobuf.DoubleValue {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.DoubleValue{
+		Value: *in,
+	}
+}
+
+// MakeBytes makes a wrapper
+func MakeBytes(in *[]byte) *google_protobuf.BytesValue {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.BytesValue{
+		Value: *in,
+	}
+}
+
+// MakeString makes a wrapper
+func MakeString(in *string) *google_protobuf.StringValue {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.StringValue{
+		Value: *in,
+	}
+}
+
+// MakeUInt32 makes a wrapper
+func MakeUInt32(in *uint32) *google_protobuf.UInt32Value {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.UInt32Value{
+		Value: *in,
+	}
+}
+
+// MakeUInt64 makes a wrapper
+func MakeUInt64(in *uint64) *google_protobuf.UInt64Value {
+	if in == nil {
+		return nil
+	}
+
+	return &google_protobuf.UInt64Value{
+		Value: *in,
+	}
 }
